b9fccf9ac33a54c6f8e4a68ada7b54e69b8c68b8
Sinedka <dennnn8888@gmail.com>
2025-10-01 10:29:01 +0300

    bug fix

--- lua/trunner/utils.lua
+++ lua/trunner/utils.lua
@@ -1,9 +1,8 @@
 local M = {}
 
 M.replaceVars = function(command, path)
-  command = command:gsub("$fileNameWithoutExt", vim.fn.fnamemodify(path, ":r"))
+  command = command:gsub("$fileNameWithoutExt", path:match("^(.*)%."))
   command = command:gsub("$fileName", path)
-
   return command
 end
 
