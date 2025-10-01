196d1551af13c711e8ff610293a5740e5192950a
Sinedka <dennnn8888@gmail.com>
2025-10-01 10:49:00 +0300

    some fixes

--- lua/trunner/utils.lua
+++ lua/trunner/utils.lua
@@ -1,8 +1,12 @@
 local M = {}
 
 M.replaceVars = function(command, path)
+  local lastdot = path:match(".*()%.") -- индекс последней точки
+  local result = path:sub(1, lastdot - 1)
+  print(result)                     --> example.text
+
   print(path:match("^(.*)%.*"));
-  command = command:gsub("$fileNameWithoutExt", path:match("^(.*)%.*"))
+  command = command:gsub("$fileNameWithoutExt", result)
   command = command:gsub("$fileName", path)
   return command
 end
